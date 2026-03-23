958a3ce2b3c413b088af5ecc62bfda9c060b1027
HottaIssa <ostosissa@gmail.com>
2026-03-23 17:55:04 -0500

    fix: set up R2 bucket for images

--- src/main/java/com/saihoz/task_app/mapper/UserMapper.java
+++ src/main/java/com/saihoz/task_app/mapper/UserMapper.java
@@ -21,8 +21,8 @@ public class UserMapper {
                 user.getFirstName(),
                 user.getLastName(),
                 user.getRole().getDisplayName(),
-//                storageService.getPresignedUrl(user.getAvatar_url())
-                user.getAvatar_url()
+                storageService.getPresignedUrl(user.getAvatar_url())
+//                user.getAvatar_url()
         );
     }
 
@@ -33,8 +33,8 @@ public class UserMapper {
                 user.getUsername(),
                 user.getEmail(),
                 user.getFullName(),
-//                storageService.getPresignedUrl(user.getAvatar_url())
-                user.getAvatar_url()
+                storageService.getPresignedUrl(user.getAvatar_url())
+//                user.getAvatar_url()
         );
     }
 
